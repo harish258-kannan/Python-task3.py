e564007fe12f50343f526f21dee43131792e3e35
harish258-kannan <harishkannan258@gmail.com>
2026-03-18 21:05:10 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,49 @@
+for i in range(1, 51):    
+    print(i) 
+
+
+for i in range(2, 101, 2):   
+    print(i)
+
+
+for i in range(1, 101, 2):   
+    print(i)
+
+
+
+for i in range(1, 11):          
+    print(7 * i)
+
+
+
+total = 0                        
+for i in range(1, 101):          
+    total += i                   
+print(total)
+
+
+
+for i in range(50, 0, -1):       
+    print(i)
+
+
+
+count = 0
+for i in range(1, 101):
+    if i % 3 == 0:              
+        count += 1
+print(count)
+
+
+for i in range(1, 11):
+    print(i * i)
+
+
+for i in range(1, 11):
+    print(i ** 3)
+
+
+
+n = int(input("Enter number: "))   
+for i in range(1, n+1):            
+    print(i)
